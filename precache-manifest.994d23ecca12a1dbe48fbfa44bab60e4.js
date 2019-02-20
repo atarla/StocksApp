d6eaaba06217c57ce28b64d6f4ebffb9e33ed8b5
anusha <atarla@uncc.edu>
2019-02-20 13:31:10 -0500

    Updates

---
+++ precache-manifest.994d23ecca12a1dbe48fbfa44bab60e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbd7f104dbeaa259408e",
+    "url": "/StocksApp/static/css/main.deb7192f.chunk.css"
+  },
+  {
+    "revision": "bbd7f104dbeaa259408e",
+    "url": "/StocksApp/static/js/main.bbd7f104.chunk.js"
+  },
+  {
+    "revision": "8a4fc63f70d2bd2bbe7a",
+    "url": "/StocksApp/static/js/runtime~main.8a4fc63f.js"
+  },
+  {
+    "revision": "d83f99fc9c5b87ff8ba1",
+    "url": "/StocksApp/static/js/2.d83f99fc.chunk.js"
+  },
+  {
+    "revision": "dabe3c86054e4bad9add0e2bc50378b7",
+    "url": "/StocksApp/static/media/casalogo.dabe3c86.svg"
+  },
+  {
+    "revision": "42d78891ba4f69bd3a9e31527524bbec",
+    "url": "/StocksApp/index.html"
+  }
+];
